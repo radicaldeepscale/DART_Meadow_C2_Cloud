4beb3bdfc6b76153f66f0ae266881990d9e99dff
radicaldeepscale <radicaldeepscale@gmail.com>
2022-10-11 17:29:30 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/xBCXL9AyNrJ7FFZB/scene.splinecode" />
+  );
+}
